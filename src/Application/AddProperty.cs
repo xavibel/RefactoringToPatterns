314fa567e87a87641aad20acf55951fcaa1633f9
Xavi Belloso <xbelloso@voxelgroup.net>
2024-11-28 22:12:53 +0100

    AddProperty GuardClause

--- src/Application/AddProperty.cs
+++ src/Application/AddProperty.cs
@@ -36,72 +36,55 @@ public class AddProperty
     public void Execute(int id, string description, string postalCode, int price, int numberOfRooms, int squareMeters,
         int ownerId)
     {
-        Property property;
-        if (Regex.IsMatch(postalCode, @"^\d{5}$"))
+        if (!Regex.IsMatch(postalCode, @"^\d{5}$"))
+            throw new InvalidPostalCodeException(postalCode + " is not a valid postal code");
+
+        if (price < 0)
+            throw new InvalidPriceException("Price cannot be negative");
+
+        var usersAsString = ReadJSONFileContent(usersFile);
+        var users = JsonConvert.DeserializeObject<List<User>>(usersAsString);
+        
+        var user = users.FirstOrDefault(u => u.Id == ownerId);
+        if (user == null)
+            throw new InvalidUserIdException("The owner " + ownerId + " does not exist");
+
+        var propertiesAsString = ReadJSONFileContent(propertiesFile);
+        var allProperties = JsonConvert.DeserializeObject<List<Property>>(propertiesAsString) ?? new List<Property>();
+        var property = new Property(id, description, postalCode, price, numberOfRooms, squareMeters, ownerId);
+        allProperties.Add(property);
+        WritePropertiesFile(allProperties);
+
+        string alertsAsString = ReadJSONFileContent(alertsFile);
+        var alerts = JsonConvert.DeserializeObject<List<Alert>>(alertsAsString) ?? new List<Alert> { };
+
+        foreach (var alert in alerts)
         {
-            if (price >= 0)
+            if (HasToSendTheAlert(property, alert))
             {
-                string usersAsString = ReadJSONFileContent(usersFile);
-                var users = JsonConvert.DeserializeObject<List<User>>(usersAsString);
-                var user = users.FirstOrDefault(u => u.Id == ownerId);
-
-                if (user != null)
+                var userToAlert = users.FirstOrDefault(u => u.Id == alert.UserId);
+                if (userToAlert != null)
                 {
-                    string propertiesAsString = ReadJSONFileContent(propertiesFile);
-                    var allProperties = JsonConvert.DeserializeObject<List<Property>>(propertiesAsString) ??
-                                        new List<Property>();
-                    property = new Property(id, description, postalCode, price, numberOfRooms, squareMeters, ownerId);
-                    allProperties.Add(property);
-                    WritePropertiesFile(allProperties);
-
-                    string alertsAsString = ReadJSONFileContent(alertsFile);
-                    var alerts = JsonConvert.DeserializeObject<List<Alert>>(alertsAsString) ?? new List<Alert>();
-
-                    foreach (var alert in alerts)
+                    if (alert.AlertType.Equals(AlertType.EMAIL.ToString(), StringComparison.OrdinalIgnoreCase))
                     {
-                        if (HasToSendTheAlert(property, alert))
-                        {
-                            var userToAlert = users.FirstOrDefault(u => u.Id == alert.UserId);
-                            if (userToAlert != null)
-                            {
-                                if (alert.AlertType.Equals(AlertType.EMAIL.ToString(),
-                                        StringComparison.OrdinalIgnoreCase))
-                                {
-                                    emailSender.SendEmail(new Email("noreply@codium.team", userToAlert.Email,
-                                        "There is a new property at " + property.PostalCode,
-                                        "More information at https://properties.codium.team/" + property.Id));
-                                }
-                                else if (alert.AlertType.Equals(AlertType.SMS.ToString(),
-                                             StringComparison.OrdinalIgnoreCase))
-                                {
-                                    smsSender.SendSMSAlert(new SmsMessage(userToAlert.PhoneNumber,
-                                        "There is a new property at " + property.PostalCode +
-                                        ". More information at https://properties.codium.team/" + property.Id));
-                                }
-                                else if (alert.AlertType.Equals(AlertType.PUSH.ToString(),
-                                             StringComparison.OrdinalIgnoreCase))
-                                {
-                                    pushSender.SendPushNotification(new PushMessage(userToAlert.PhoneNumber,
-                                        "There is a new property at " + property.PostalCode +
-                                        ". More information at https://properties.codium.team/" + property.Id));
-                                }
-                            }
-                        }
+                        emailSender.SendEmail(new Email("noreply@codium.team", userToAlert.Email,
+                            "There is a new property at " + property.PostalCode,
+                            "More information at https://properties.codium.team/" + property.Id));
+                    }
+                    if (alert.AlertType.Equals(AlertType.SMS.ToString(), StringComparison.OrdinalIgnoreCase))
+                    {
+                        smsSender.SendSMSAlert(new SmsMessage(userToAlert.PhoneNumber,
+                            "There is a new property at " + property.PostalCode +
+                            ". More information at https://properties.codium.team/" + property.Id));
+                    }
+                    if (alert.AlertType.Equals(AlertType.PUSH.ToString(), StringComparison.OrdinalIgnoreCase))
+                    {
+                        pushSender.SendPushNotification(new PushMessage(userToAlert.PhoneNumber,
+                            "There is a new property at " + property.PostalCode +
+                            ". More information at https://properties.codium.team/" + property.Id));
                     }
                 }
-                else
-                {
-                    throw new InvalidUserIdException("The owner " + ownerId + " does not exist");
-                }
             }
-            else
-            {
-                throw new InvalidPriceException("Price cannot be negative");
-            }
-        }
-        else
-        {
-            throw new InvalidPostalCodeException(postalCode + " is not a valid postal code");
         }
 
         if (logger != null)
